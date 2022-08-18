894c736947301448faf848898a4cdaa13314e3ec
Kaitlyn <katykat5099@gmail.com>
2022-08-18 18:24:50 -0500

    Pokeball Last Used Nuzlocke Fix

--- src/battle_interface.c
+++ src/battle_interface.c
@@ -36,7 +36,6 @@
 #include "tx_randomizer_and_challenges.h"
 #include "constants/battle_config.h"
 #include "constants/items.h"
-#include "tx_difficulty_challenges.h"
 #include "battle_setup.h" //tx_difficulty_challenges
 
 struct TestingBar
@@ -3343,13 +3342,13 @@ bool32 CanThrowLastUsedBall(void)
         return FALSE;
     if (!CheckBagHasItem(gLastThrownBall, 1))
         return FALSE;
-    if (gSaveBlock1Ptr->txRandNuzlocke && NuzlockeIsCaptureBlocked) //tx_difficulty_challenges
+    if (gSaveBlock1Ptr->tx_Challenges_Nuzlocke && NuzlockeIsCaptureBlocked) //tx_difficulty_challenges
         return FALSE;
-    if (gSaveBlock1Ptr->txRandNuzlocke && NuzlockeIsSpeciesClauseActive == 2) //already have THIS_mon
+    if (gSaveBlock1Ptr->tx_Challenges_Nuzlocke && NuzlockeIsSpeciesClauseActive == 2) //already have THIS_mon
         return FALSE;
-    if (gSaveBlock1Ptr->txRandTypeChallenge && TypeChallengeCaptureBlocked) //pkmn not of the TYPE CHALLENGE type
+    if (gSaveBlock1Ptr->tx_Challenges_OneTypeChallenge && TypeChallengeCaptureBlocked) //pkmn not of the TYPE CHALLENGE type
         return FALSE;
-    if (gSaveBlock1Ptr->txRandNuzlocke && NuzlockeIsSpeciesClauseActive)
+    if (gSaveBlock1Ptr->tx_Challenges_Nuzlocke && NuzlockeIsSpeciesClauseActive)
         return FALSE;
 
     return TRUE;
